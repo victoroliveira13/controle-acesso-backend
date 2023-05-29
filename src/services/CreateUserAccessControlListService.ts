6a46cb3bce28c55a96b70e4941adbd1a542b396d
victoroliveira13 <victor12321@hotmail.com.br>
2023-05-29 16:31:03 -0300

    refactor( acl user ): o cadastro de permissons agora mantem as ja existentes. Tambem nao eh mais possivel vincular uma permission ao user caso sua role ja a possua

--- src/services/CreateUserAccessControlListService.ts
+++ src/services/CreateUserAccessControlListService.ts
@@ -1,3 +1,4 @@
+import { Permission } from "../entities/Permission";
 import { User } from "../entities/User";
 import {
   PermissionRepository,
@@ -11,6 +12,17 @@ type UserACLRequest = {
   permissions: string[];
 };
 
+type ACLResponse = {
+  name: string;
+  description: string;
+}
+
+type ACLUserResponse = {
+  user: string;
+  roles: ACLResponse[];
+  permissions: ACLResponse[];
+}
+
 /* Vincular permission e role em user
 Pre-condicao: nao ha;
 Pos-condicao: user com novas permissions e roles;
@@ -18,30 +30,67 @@ Descricao: Recebe o id do usuario logado (token bearer), e vincula ao mesmo, as
 permissions e roles passadas.
 */
 export class CreateUserAccessControlListService {
-  async execute({
-    userId,
-    roles,
-    permissions,
-  }: UserACLRequest): Promise<User | Error> {
+  async execute({ userId, roles, permissions}: UserACLRequest): Promise<ACLUserResponse | Error> {
     const repo = UserRepository();
 
-    const user = await repo.findOne(userId);
+    const user = await repo.findOne(userId, { relations: ["roles", "permissions"] });
 
     if (!user) {
-      return new Error("User does not exists!");
+      return new Error("User does not exist!");
     }
 
-    const permissionsExists = await PermissionRepository().findByIds(
-      permissions
-    );
-
+    //Guarda apenas permissions e roles validas (nome correto)
+    const permissionsExists = await PermissionRepository().findByIds(permissions);
     const rolesExists = await RoleRepository().findByIds(roles);
 
-    user.permissions = permissionsExists;
-    user.roles = rolesExists;
+    // Observa as permissions na role do user, para impedir cadastro de uma permission que a role
+    // do user ja possua
+    const permissionsRole: ACLResponse[] = [];
+    for (const userRole of user.roles) {
+      const roleRepo = RoleRepository();
+      const roleEntity = await roleRepo.findOne({ name: userRole.name}, { relations: ["permissions"] });
+    
+      const rolePermissions: ACLResponse[] = roleEntity.permissions.map((permission: Permission) => ({
+        name: permission.name,
+        description: permission.description,
+      }));
+    
+      permissionsRole.push(...rolePermissions);
+    }
+    
+    //Mantem ACL ja existentes, e adciona as permissions que o user e sua respectiva role contem
+    const permissionsToAdd: Permission[] = [];
+    const rolesToAdd = [];
+    const skippedPermissions: Permission[] = []; // armazenar as permissions que foram ignoradas
+    for (const newPermission of permissionsExists) {
+      // Verifica se a permission nao esta presente nem em `user.permissions` nem em `permissionsRole`
+      if (!user.permissions.find((permission) => permission.name === newPermission.name) &&
+          !permissionsRole.find((permission) => permission.name === newPermission.name)) {
+        permissionsToAdd.push(newPermission);
+      } else {
+        skippedPermissions.push(newPermission);
+      }
+    }
+    for (const newRole of rolesExists) {
+      if (!user.roles.find((role) => role.name === newRole.name)) {
+        rolesToAdd.push(newRole);
+      }
+    }
+    user.permissions.push(...permissionsToAdd);
+    user.roles.push(...rolesToAdd);  
 
-    repo.save(user);
+    if (permissionsToAdd.length === 0 && skippedPermissions.length > 0) {
+      const errorMessage = `Error: No new permissions added because the following permissions already exist in user's roles: ${skippedPermissions.map(permission => permission.name).join(", ")}`;
+      return new Error(errorMessage);
+    }
 
-    return user;
+    await repo.save(user);
+    const aclUserResponse: ACLUserResponse = {
+      user: user.username,
+      roles: user.roles,
+      permissions: user.permissions,
+    };
+
+    return aclUserResponse;
   }
 }
